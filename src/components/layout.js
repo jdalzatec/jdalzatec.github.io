43cf6eafba663ea2cd4f43e3784c24a8a96a7ea9
jdalzatec <jdalzatec@unal.edu.co>
2019-04-15 18:03:19 -0500

    Adding flexbox to the header

--- src/components/layout.js
+++ src/components/layout.js
@@ -4,32 +4,32 @@ import myicon from "../images/icon.png"
 
 
 const ListLink = props => (
-    <li style={{ display: "inline-block", marginRight: "1rem" }}>
+    <li style={{ display: "inline", marginLeft: "1rem"}}>
         <Link to={props.to}>{props.children}</Link>
     </li>
 )
 
 export default ({ children }) => (
     <div>
-            <header id="top-panel">
-                <div id="links">
-                    <h3 style={{ display: "inline" }}>
-                        <Link to="/">
-                            <div style={{display: "inline", alignItems: "middle"}}>
-                                <img style={{width: "5%", float: "left", margin: "0px"}} src={myicon} alt="icon" />
-                                <span style={{marginLeft: "10px"}}>jdalzatec's blog</span>
-                            </div>
-                        </Link>
-                    </h3>
-
-                    <ul id="navigation-links" style={{ listStyle: "none", float: "right", margin: 0 }}>
-                        <ListLink to="/posts/">Posts</ListLink>
-                        {/* <ListLink to="/projects/">Projects</ListLink> */}
-                        <ListLink to="/contact/">Contact</ListLink>
-                    </ul>
+        <div id="top-panel">
+            <div id="left-links">
+                <div id="home-link">
+                    <img style={{width: "8%", margin: "0px"}} src={myicon} alt="icon" />
+                    <Link to="/">
+                        <span style={{ marginLeft: "10px", fontSize: "1.2rem" }}>jdalzatec's blog</span>
+                    </Link>
                 </div>
-            </header>
+            </div>
 
-            {children}
+            <div>
+                <ul style={{margin: "0px", padding: "0px"}}>
+                    <ListLink to="/posts/">Posts</ListLink>
+                    {/* <ListLink to="/projects/">Projects</ListLink> */}
+                    <ListLink to="/contact/">Contact</ListLink>
+                </ul>
+            </div>
+        </div>
+
+        {children}
     </div>
 )
