a28e0014cdfe734407691e4f6b90215bfc0d6a2d
jdalzatec <jdalzatec@unal.edu.co>
2019-04-15 11:51:19 -0500

    Going forward 5.0 with the scaffolding

---
+++ src/components/layout.js
@@ -0,0 +1,33 @@
+import React from "react"
+import { Link } from "gatsby"
+
+
+const ListLink = props => (
+    <li style={{ display: "inline-block", marginRight: "1rem" }}>
+        <Link to={props.to}>{props.children}</Link>
+    </li>
+)
+
+export default ({ children }) => (
+    <div>
+        <header id="top-panel">
+            <div id="links">
+                <h3 style={{ display: "inline" }}>
+                    <Link to="/" style={{ color: "#1d3557" }}>
+                        jdalzatec's blog
+                    </Link>
+                </h3>
+
+                <ul id="navigation-links" style={{ listStyle: "none", float: "right", margin: 0 }}>
+                    <ListLink to="/posts/">Posts</ListLink>
+                    <ListLink to="/projects/">Projects</ListLink>
+                    <ListLink to="/contact/">Contact</ListLink>
+                </ul>
+            </div>
+        </header>
+
+        <body>
+            {children}
+        </body>
+    </div>
+)
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,27 +1,30 @@
 import React from "react"
 import mypicture from "../images/jdalzatec.jpg"
+import Layout from "../components/layout"
 
 import "../styles/global.css"
 
 export default () => (
-  <div id="about-me-container">
-    <div id="div-about-me">
-      <h1 style={{textAlign: "center"}}>Nice to meet you !</h1>
-      <br />
-      <img id="mypicture" src={mypicture} alt="me" />
-      <div id="description">
-        <p>
+  <Layout>
+    <div id="about-me-container">
+      <div id="div-about-me">
+        <h2 style={{textAlign: "center"}}>Nice to meet you !</h2>
+        <br />
+        <img id="mypicture" src={mypicture} alt="me" />
+        <div id="description">
+          <p>
 
-        My name is <strong style={{ color: "#1d3557"}}>Juan David Alzate Cardona</strong>.
-        I am a Physics Engineer, highly passionate in computational physics.
-        I am also an amateur programmer with a high interest in Data Science.
-        I am from <a target="_" href="https://en.wikipedia.org/wiki/Manizales">Manizales</a>, <a target="_" href="https://en.wikipedia.org/wiki/Caldas_Department">Caldas</a>, <a target="_" href="https://en.wikipedia.org/wiki/Colombia">Colombia</a>.
-  
-        For more details, see my ré­sumé.
+          My name is <strong style={{ color: "#1d3557"}}>Juan David Alzate Cardona</strong>.
+          I am a Physics Engineer, highly passionate in computational physics.
+          I am also an amateur programmer with a high interest in Data Science.
+          I am from <a target="_" href="https://en.wikipedia.org/wiki/Manizales">Manizales</a>, <a target="_" href="https://en.wikipedia.org/wiki/Caldas_Department">Caldas</a>, <a target="_" href="https://en.wikipedia.org/wiki/Colombia">Colombia</a>.
+    
+          For more details, see my ré­sumé.
 
-        </p>
+          </p>
+        </div>
+        
       </div>
-      
     </div>
-  </div>
+  </Layout>
 )
